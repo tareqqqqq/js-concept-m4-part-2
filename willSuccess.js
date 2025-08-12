743109745da290049963c71dc94de14b1502ce62
unknown <tareqmahmudkhan187@gmail.com>
2025-08-12 22:30:03 +0600

    willSuccess solved

--- willSuccess.js
+++ willSuccess.js
@@ -3,14 +3,34 @@ function willSuccess(marks){
         return 'invalid'
     }
     
-
+        let passCount=0;
+        
     for(const mark of marks){
         if(mark>=50){
-            return true
-        }else{
-            return false
+            passCount++
         }
-    }
+ 
+    } 
+     let failCount=marks.length-passCount
+  let result=passCount>failCount
+  return result
 }
 
-console.log(willSuccess([50,50,50]));
+console.log(willSuccess('100,100'));
+
+
+// function willSuccess(marksArray) {
+//   let passedCount = 0;
+
+//   for (let singleMark of marksArray) {
+//     if (singleMark >= 50) passedCount++;
+//   }
+
+//   let failCount = marksArray.length - passedCount;
+
+// 	let result = passedCount > failCount; // true / false
+//   return result;
+// }
+
+// let sampleMarks = [50, 49, 49];
+// console.log(willSuccess(sampleMarks));
